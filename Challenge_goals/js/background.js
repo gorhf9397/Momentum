51a6f147c20447cea8c906877bf108f08f18490b
gorhf9397 <108771927+gorhf9397@users.noreply.github.com>
2022-08-26 00:03:05 +0900

    Delete Challenge_goals directory

--- Challenge_goals/js/background.js
+++
@@ -1,8 +0,0 @@
-const backimg = ["backimg1.jpg" , "backimg2.jpg" , "backimg3.jpg"];
-
-const chosenImage = backimg[Math.floor(Math.random() * backimg.length)];
-
-const bgimg = document.createElement("img");
-bgimg.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgimg);
